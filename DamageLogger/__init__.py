1d7e7672dec68ae50835b22314958733109003b7
PyrexBLJ <49227284+PyrexBLJ@users.noreply.github.com>
2025-05-19 21:28:43 -0400

    damage logger 1.0.1
    use the proper damage cap as the filter value, not the one i assumed was the same from bl2
    setting only my damage to off will actually let things print now
    
    add autobear, iron cub, clone and flak's pet damage to only my damage mode

--- DamageLogger/__init__.py
+++ DamageLogger/__init__.py
@@ -1,6 +1,6 @@
 from typing import Any #type: ignore
 import unrealsdk #type: ignore
-from mods_base import get_pc, hook, BoolOption, SliderOption, build_mod #type: ignore
+from mods_base import get_pc, hook, BoolOption, build_mod #type: ignore
 from unrealsdk.hooks import Type #type: ignore
 from unrealsdk.unreal import BoundFunction, UObject, WrappedStruct #type: ignore
 
@@ -8,11 +8,23 @@ onlyCap: BoolOption = BoolOption("Only Log Above Damage Cap", False, "Yes", "No"
 onlyMyDamage: BoolOption = BoolOption("Only My Damage", True, "Yes", "No", description="Only log damage dealt by the player")
 roundTo2: BoolOption = BoolOption("Round Damage To 2 Decimal Places", True, "Yes", "No", description="Cleans up the log a lil bit")
 
+mydamagedealers: list = ["DigiClone", "IronBear", "Pet", "IronCub"]
+
+def doneByMe(instigator: UObject) -> bool:
+    global mydamagedealers
+    if instigator == get_pc():
+        return True
+    for dealer in mydamagedealers:
+        if dealer in str(instigator):
+            return True
+    
+    return False
+
 @hook("/Script/GbxGameSystemCore.DamageComponent:ReceiveAnyDamage", Type.POST)
 def enemyTookDamage(obj: UObject, args: WrappedStruct, _3: Any, _4: BoundFunction) -> None:
-    if onlyCap.value and float(args.Damage) >= 999999999.0 or not onlyCap.value:
+    if onlyCap.value and float(args.Damage) >= 1073741823.0 or not onlyCap.value:
         damagesource = str(args.DamageSource).split("_")
-        if onlyMyDamage.value and args.InstigatedBy == get_pc():
+        if onlyMyDamage.value and doneByMe(args.InstigatedBy) or not onlyMyDamage.value:
             if args.Details.bWasCrit:
                 print(f"Critical {damagesource[-2]} {args.DamageType.DamageIconFrameName} Damage: {round(args.Damage, 2) if roundTo2.value else args.Damage}")
             else:
